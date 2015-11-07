722d4617fbd3bb0c0b3a2af2b308e4cd5fce4d9b
Ashley Keller <ashleykeller@ashashash.local>
2015-11-06 21:38:11 -0500

    Completed initial React setup in main.js file and created first component - the storePicker

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+/*
+
+StorePicker
+
+*/
+
+var StorePicker = React.createClass({
+
+    render : function () {
+        return (
+            <p>hello</p>
+        )
+    }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
