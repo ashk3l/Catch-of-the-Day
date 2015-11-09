5ea1fc0d043ca23e914c8cd3adc9e0bfc033b5d6
Ashley Keller <ashleykeller@ashashash.local>
2015-11-08 22:04:45 -0500

    Setting up App, Header, Order and Inventory components. Changing render to show App instead of Storepicker for now.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,64 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
 /*
+  App
+*/
 
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+      <Order/>
+      <Inventory/>
+      </div>
+    )
+  }
+
+});
+
+/*
+  Header
+  <Header/>
+*/
+var Header = React.createClass({
+    render : function() {
+      return (
+        <p>Header</p>
+      )
+    }
+});
+
+/*
+  Order
+  <Order/>
+*/
+var Order = React.createClass({
+    render : function() {
+      return (
+        <p>Order</p>
+      )
+    }
+});
+
+/*
+  Inventory
+  <Inventory/>
+*/
+var Inventory = React.createClass({
+    render : function() {
+      return (
+        <p>Inventory</p>
+      )
+    }
+});
+
+/*
 StorePicker
-
 */
 
 var StorePicker = React.createClass({
@@ -19,4 +74,4 @@ var StorePicker = React.createClass({
     }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
